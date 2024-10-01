fa8d6a74be17d0605e7782e471044d2a5f6c032c
Guitarband <preetishchoudhary@gmail.com>
2024-10-01 14:38:34 +1000

    Added very basic textarea

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import WebEditor from './components/WebEditor'
 import './App.css'
 
 function App() {
@@ -17,9 +16,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <WebEditor />
     </>
   )
 }

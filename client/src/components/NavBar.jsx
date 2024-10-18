947871b50641e7dd3700fd1c81962d1cf0fa316a
Guitarband <preetishchoudhary@gmail.com>
2024-10-18 18:43:42 +1100

    Pretty much completed home page

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+
+function NavBar() {
+    return (
+        <div>
+            <nav className={"navbar"}>
+                <ul className={"nav-list"}>
+                    <li>
+                        <a href={"/problems"}>Problems</a>
+                    </li>
+                    <li>
+                        <a id={"navbarNextToMid"} href={"/courses"}>Courses</a>
+                    </li>
+                    <li>
+                        <a id={"navbarMid"} href={"/"}>PyCourses</a>
+                    </li>
+                    <li>
+                        <a id={"navbarNextToMid"} href={"/ide"}>Playground</a>
+                    </li>
+                    <li>
+                        <a href={"/contact"}>Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default NavBar
